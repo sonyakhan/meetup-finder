1a1dcc5715f0938da376f7dc35a41383f2e2d254
Sonya Khan <sonya.khan.cs@gmail.com>
2017-05-10 17:33:19 -0400

    push to heroku

--- app.js
+++ app.js
@@ -4,4 +4,5 @@ var app = express();
 app.use(express.static(__dirname + '/app'));
 
 var port = process.env.PORT || 3000;
+console.log("Express server running on " + port);
 app.listen(process.env.PORT || port);
